3e9ab6565604139aa9d119a394ef6c1efd78e009
Microhive <microhive@outlook.com>
2022-07-27 13:27:48 +0100

    Added initial AppVersionInfo integration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using EilerWebApp.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSingleton<AppVersionInfo>();
 
 var app = builder.Build();
 
