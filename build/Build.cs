fa8881e9a65603785115d08319d021a6c486b676
Microhive <microhive@outlook.com>
2022-08-02 21:59:03 +0100

    Configured nuke build with Clean, Restore, and Compile targets

--- build/Build.cs
+++ build/Build.cs
@@ -3,14 +3,20 @@ using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
+[DotNetVerbosityMapping]
+[UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -24,27 +30,43 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+    //[GitRepository] readonly GitRepository GitRepository;
+    //[GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    [Parameter]
+    readonly string publishOutput = IsServerBuild
+        ? Configuration.Release
+        : Configuration.Debug;
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore()
+                .SetOutputDirectory(OutputDirectory));
         });
-
-    Target Deploy => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
-
 }
