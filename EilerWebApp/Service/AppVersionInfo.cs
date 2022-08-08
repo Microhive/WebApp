58801555e6ca2d683f1137c481b1bfd3c9f2b98a
Microhive <microhive@outlook.com>
2022-08-08 22:20:32 +0100

    _Layout.cshtml now contains links to git branch and commit

--- EilerWebApp/Service/AppVersionInfo.cs
+++ EilerWebApp/Service/AppVersionInfo.cs
@@ -4,102 +4,34 @@ namespace EilerWebApp.Service
 {
     public class AppVersionInfo
     {
-        private static readonly string _buildFileName = ".buildinfo.json";
-        private string _buildFilePath;
-        private string _buildNumber;
-        private string _buildId;
-        private string _gitHash;
-        private string _gitShortHash;
-
-        public AppVersionInfo(IHostEnvironment hostEnvironment)
+        public AppVersionInfo()
         {
-            _buildFilePath = Path.Combine(hostEnvironment.ContentRootPath, _buildFileName);
-        }
+            var version = "1.Branch.dev.Sha.4a99b73ae9502b35af8c266dc398b7f12a95ca83"; // Dummy version for local dev
 
-        public string BuildNumber
-        {
-            get
+            var appAssembly = typeof(AppVersionInfo).Assembly;
+            var infoVerAttr = (AssemblyInformationalVersionAttribute)appAssembly
+                .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute)).FirstOrDefault();
+
+            if (infoVerAttr != null && infoVerAttr.InformationalVersion.Length > 6)
             {
-                // Build number format should be yyyyMMdd.# (e.g. 20200308.1)
-                if (string.IsNullOrEmpty(_buildNumber))
-                {
-                    if (File.Exists(_buildFilePath))
-                    {
-                        var fileContents = File.ReadLines(_buildFilePath).ToList();
-
-                        // First line is build number, second is build id
-                        if (fileContents.Count > 0)
-                        {
-                            _buildNumber = fileContents[0];
-                        }
-                        if (fileContents.Count > 1)
-                        {
-                            _buildId = fileContents[1];
-                        }
-                    }
-
-                    if (string.IsNullOrEmpty(_buildNumber))
-                    {
-                        _buildNumber = DateTime.UtcNow.ToString("yyyyMMdd") + ".0";
-                    }
-
-                    if (string.IsNullOrEmpty(_buildId))
-                    {
-                        _buildId = "123456";
-                    }
-                }
-
-                return _buildNumber;
+                // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
+                version = infoVerAttr.InformationalVersion;
             }
+
+            var versionParts = version.Split('.');
+
+            this.commitGitNumber = versionParts[0];
+            this.gitBranchName = versionParts[2];
+            this.longGitHash = versionParts[4];
+            this.shortGitHash = versionParts[4].Substring(versionParts[4].Length - 6, 6);
         }
 
-        public string BuildId
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_buildId))
-                {
-                    var _ = BuildNumber;
-                }
+        public string longGitHash { get; }
 
-                return _buildId;
-            }
-        }
+        public string shortGitHash  { get; }
 
-        public string GitHash
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_gitHash))
-                {
-                    var version = "1.0.0+LOCALBUILD"; // Dummy version for local dev
-                    var appAssembly = typeof(AppVersionInfo).Assembly;
-                    var infoVerAttr = (AssemblyInformationalVersionAttribute)appAssembly
-                        .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute)).FirstOrDefault();
+        public string gitBranchName  { get; }
 
-                    if (infoVerAttr != null && infoVerAttr.InformationalVersion.Length > 6)
-                    {
-                        // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
-                        version = infoVerAttr.InformationalVersion;
-                    }
-                    _gitHash = version.Substring(version.IndexOf('+') + 1);
-
-                }
-
-                return _gitHash;
-            }
-        }
-
-        public string ShortGitHash
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(_gitShortHash))
-                {
-                    _gitShortHash = GitHash.Substring(GitHash.Length - 6, 6);
-                }
-                return _gitShortHash;
-            }
-        }
+        public string commitGitNumber { get; }
     }
 }
